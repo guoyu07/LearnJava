548390e8543b3d82f785a9a376ded9a1488be09d
Vivek Ratkalkar <vratkalkar@live.com>
2016-01-09 14:26:44 -0500

    Add second compound boolean expression

--- ShallowGrandmother.java
+++ ShallowGrandmother.java
@@ -5,21 +5,29 @@ public class ShallowGrandmother{
 		Scanner keyboard = new Scanner(System.in);
 
 		int age;
-		double income, looks;
-		boolean allowed;
+		double income, looks, happy;
+		boolean allowed, allowed2;
 
 		System.out.print( "Enter your age: " );
 		age = keyboard.nextInt();
 
-		System.out.print ("Enter your yearly income: ");
+		System.out.print("Enter your yearly income: ");
 		income = keyboard.nextDouble();
 
 		System.out.print( "How good looking are you, on a scale from 0.0 to 10.0? ");
 		looks = keyboard.nextDouble();
 
-		allowed = ( age > 25 && age < 40 && (income > 5000 || looks >= 8.5));
+		System.out.print( "How happy do you make her, on a scale from 0.0 to 10.0? ");
+		happy = keyboard.nextDouble();
+
+		allowed = (age > 25 && age < 40 && (income > 5000 || looks >= 8.5));
+		allowed2 = (age > 20 && age < 35 && (income > 10000 || looks >= 8.2) && happy >= 8);
 
 		System.out.println( "Allowed to date my grandchild? " + allowed);
+		System.out.println( "Allowed to date my grandchild? " + allowed2);
+
+
+
 
 	}
 
