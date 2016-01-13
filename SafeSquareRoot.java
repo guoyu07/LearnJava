3d64c2f7be386c05e4790c2e97983cb92ce2ec2f
Vivek Ratkalkar <vratkalkar@live.com>
2016-01-13 11:00:59 -0500

    Add Square root with loops for error checking

--- SafeSquareRoot.java
+++ SafeSquareRoot.java
@@ -5,6 +5,17 @@ public class SafeSquareRoot {
 		Scanner keyboard = new Scanner(System.in);
 
 		double x, y;
+		String response;
+
+		System.out.print("Are you ready?!?");
+		response = keyboard.next();
+
+		while ( !response.equals("YES!")){
+			System.out.println("Not the answer we're looking for!");
+			System.out.print("Now, are your ready?!?");
+
+			response = keyboard.next();
+		}
 
 		System.out.print("Give me a number, and I'll find it's square root. ");
 		System.out.print("(No negatives, please.) ");
