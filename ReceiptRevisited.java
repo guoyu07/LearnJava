dd20862271f23400ed22e4142e1705ba88e763f5
Vivek Ratkalkar <vratkalkar@live.com>
2016-01-22 17:36:39 -0500

    Update to handle user input

--- ReceiptRevisited.java
+++ ReceiptRevisited.java
@@ -1,32 +1,48 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class ReceiptRevisited {
 	public static void main ( String[] args ){
 	
 		PrintWriter fileout;
+		double ppg = 2.45;
+		double total;
+		int gallons;
+		Scanner keyboard = new Scanner(System.in);
 
 		try {
 			fileout = new PrintWriter("receipt.txt");
 		}
 		catch ( IOException err ) {
+
 			System.out.println("Sorry, I can't open 'receipt.txt' for writing");
 			System.out.println("Maybe the file exists and is read-only?");
 			fileout = null;
 			System.exit(1);
 		}
 
-		fileout.println( "+-----------------------+");
-		fileout.println( "|                       |");
-		fileout.println( "|       Corner Store    |");
-		fileout.println( "|    2014-06-25 04:38PM |");
-		fileout.println( "|                       |");
-		fileout.println( "|   Gallons: 12.464     |");
-		fileout.println( "| Price/gallon: $ 3.459 |");
-		fileout.println( "|                       |");
-		fileout.println( "| Fuel Total: $ 43.11   |");
-		fileout.println( "|                       |");
-		fileout.println( "+-----------------------+");
+  		// Display and print info to terminal
+		System.out.println("Gas costs " + ppg + " per gallon");
+		System.out.println("How many gallons would you like to purchase?:");
+
+		gallons = keyboard.nextInt();
+		total = gallons * ppg;
+		
+		System.out.println("Total cost: " + total);
+
+		// Write to receipt.txt
+		fileout.println( "+------------------------+");
+		fileout.println( "|                         ");
+		fileout.println( "|       Corner Store      ");
+		fileout.println( "|    2014-06-25 04:38PM   ");
+		fileout.println( "|                         ");
+		fileout.println( "|   Gallons: "+gallons+"  ");
+		fileout.println( "|   Price/gallon: "+ppg+" ");
+		fileout.println( "|                         ");
+		fileout.println( "|  Fuel Total: "+total+"  ");
+		fileout.println( "|                         ");
+		fileout.println( "+------------------------+");
 		fileout.close();
 	  }
 	}
