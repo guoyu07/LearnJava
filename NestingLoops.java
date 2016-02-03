2bf9b2d3ed6c9ce3c3ab32734a89239713560b45
Vivek Ratkalkar <vratkalkar@live.com>
2016-02-03 14:48:11 -0500

    Add println to second loop

---
+++ NestingLoops.java
@@ -0,0 +1,25 @@
+public class NestingLoops {
+	public static void main( String[] args ){
+		//this is #1, named "CN"
+		for ( int n = 1; n <= 3; n++ ) {
+		 for ( char c = 'A'; c <= 'E'; c++ ) {
+			{
+				System.out.println( c + " " + n );
+			}
+		 } 
+	 }
+
+		System.out.println("\n");
+
+		//This is #2, named "AB"
+		for ( int a = 1; a <= 3; a++ ) {
+			for ( int b = 1; b <= 3; b++ ){
+				System.out.println( "(" + a + "," + b + ") " );
+			}
+
+			// Add a line of code here
+			System.out.println();
+		}
+		System.out.println("\n");
+	}
+}
