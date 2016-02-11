c0cbf09d33815b908f4848656e128e967c762ef3
Vivek Ratkalkar <vratkalkar@live.com>
2016-02-11 13:10:56 -0500

    Add variable type to Address class

--- MailingAddresses.java
+++ MailingAddresses.java
@@ -1,8 +1,8 @@
 class Address {
-	String street;
-	String city;
-	String state;
-	int zip;
+ 	public String street;
+ 	public String city;
+ 	public String state;
+ 	public int zip;
 }
 
 public class MailingAddresses {
